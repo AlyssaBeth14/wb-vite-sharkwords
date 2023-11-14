6fb82ab45f592d34ebe80d37bb8e8b820c847bf6
Alyssa Burdett <alyssa.burdett14@gmail.com>
2023-11-14 08:55:13 -0700

    second commit

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import './style.css';
+import getRandomWord from './src/randomWord.js';
+import setSharkImage from './src/sharkImage.js';
+import setupWord from './src/word.js';
+import isLetterInWord from './src/word.js'
+import revealLetterInWord from './src/word.js';
 
 document.querySelector('#app').innerHTML = `
   <section id="shark-img"></section>
@@ -12,10 +17,29 @@ document.querySelector('#app').innerHTML = `
 
 const initSharkwords = () => {
   let numWrong = 0;
-  const word = 'word';
+  const word = getRandomWord()
+  setSharkImage(document.querySelector('#shark-img'), numWrong);
+  setupWord(document.querySelector('#word-container'), word);
 
+  const handleGuess = (guessEvent, letter) => {
+    const button = guessEvent.target
+    button.setAttribute('disabled', true)
+    if(isLetterInWord(letter)){
+      revealLetterInWord(letter)
+    } else {
+      numWrong += 1;
+      setSharkImage(sharkImgEl, numWrong);
+    }
+  };
+  
+  setupGuesses(document.querySelector('#letter-buttons'), handleGuess);
+  
+  
   // for debugging:
   console.log(`[INFO] Correct word is: ${word}`);
 };
 
-initSharkwords();
+
+
+initSharkwords() 
+
